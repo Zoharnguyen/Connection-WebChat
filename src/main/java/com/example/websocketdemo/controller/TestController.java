bc9f8647f35c71bc18f11717615793a9eaf3d078
TrungNQ24 <TrungNQ24@fsoft.com.vn>
2019-11-07 18:10:48 +0700

    Test jsp

---
+++ src/main/java/com/example/websocketdemo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.websocketdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+  @RequestMapping("/page1")
+  public String viewPage1() {
+    return "viewPage1";
+  }
+
+}
